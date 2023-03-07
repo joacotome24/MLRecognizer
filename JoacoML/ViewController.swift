9abec2191e4b87cfd6eda5cda6f94add477263c7
joacotome24 <joaco.tome@gmail.com>
2023-03-07 12:22:42 -0300

    added project to github

---
+++ JoacoML/ViewController.swift
@@ -0,0 +1,81 @@
+
+
+import UIKit
+import CoreML
+import Vision
+import Social
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    var classificationResults : [VNClassificationObservation] = []
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        typeLabel.isHidden = true
+        imagePicker.delegate = self
+        self.navigationItem.title = "Joaco Machine Learning"
+        self.navigationController?.navigationBar.barTintColor = UIColor.green
+        self.navigationController?.navigationBar.isTranslucent = false
+    }
+    
+    func detect(image: CIImage) {
+        
+        // Load the ML model through its generated class
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("can't load ML model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first
+                else {
+                    fatalError("unexpected result type from VNCoreMLRequest")
+            }
+            
+            DispatchQueue.main.async {
+                self.typeLabel.isHidden = false
+                self.typeLabel.text = topResult.identifier.description
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.originalImage] as? UIImage {
+            
+            imageView.image = image
+            imagePicker.dismiss(animated: true, completion: nil)
+            guard let ciImage = CIImage(image: image) else {
+                fatalError("couldn't convert uiimage to CIImage")
+            }
+            detect(image: ciImage)
+        }
+    }
+    
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+    return input.rawValue
+}
